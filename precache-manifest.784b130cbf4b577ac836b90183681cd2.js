df5d18046025211d1412e75b814c21bc06cc70fd
GAlekhya123 <alekhyaganji440@gmail.com>
2020-04-29 15:02:13 +0530

    Updates

---
+++ precache-manifest.784b130cbf4b577ac836b90183681cd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da44f29fb7242297990aeab479969a52",
+    "url": "/alekhyaprofile/index.html"
+  },
+  {
+    "revision": "de3e46272f19d1a6aaf2",
+    "url": "/alekhyaprofile/static/css/main.4d7ad4fd.chunk.css"
+  },
+  {
+    "revision": "a61f3f1147db33bed5f9",
+    "url": "/alekhyaprofile/static/js/2.729f334c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/alekhyaprofile/static/js/2.729f334c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de3e46272f19d1a6aaf2",
+    "url": "/alekhyaprofile/static/js/main.6b398118.chunk.js"
+  },
+  {
+    "revision": "e6fcba781320e25820e3",
+    "url": "/alekhyaprofile/static/js/runtime-main.a9735d6e.js"
+  }
+]);
